6c2f699c1e3ca937eb10971813df38d4cb5cae29
Rodolphe <rodolphe.parlouer@gmail.com>
2026-02-25 10:41:34 +0100

    push

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import './main'
 function App() {
+const login = document.querySelector('.HeaderImg')
+const quit = document.querySelectorAll('.Log-btn') 
+const visible = document.querySelector('.LogFull')
 
-  return (
-    
+login.addEventListener('click', () => {
+    visible.style.display = 'flex'
+})
 
-  )
-}
+quit.forEach (l => {
+    l.addEventListener('click', () => {
+        visible.style.display = 'none'
+    } )
+})
+
+
+} 
+ 
+
 
 export default App
